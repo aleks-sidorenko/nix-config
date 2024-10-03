23c0b01ad7550f471c7b62bd18c5cf32c816fd12
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2024-10-03 14:21:43 +0300

    fix: monitors

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -2,12 +2,10 @@
   i18n = {
     defaultLocale = lib.mkDefault "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_TIME = lib.mkDefault "ua_UA.UTF-8";
+      LC_TIME = lib.mkDefault "en_US.UTF-8";
     };
     supportedLocales = lib.mkDefault [
       "en_US.UTF-8/UTF-8"
-      "ru_UA.UTF-8/UTF-8"
-      "uk_UA.UTF-8/UTF-8"
     ];
   };
   location.provider = "geoclue2";
--- users/alexander/zeus.nix
+++ users/alexander/zeus.nix
@@ -5,28 +5,30 @@
   imports = [
     ./global
     ./features/desktop/hyprland
-    ./features/desktop/wireless
+    #./features/desktop/wireless
     ./features/rgb
     ./features/productivity
     ./features/pass
   ];
 
-  # Purple
-  wallpaper = pkgs.wallpapers.aenami-lost-in-between;
+  # Green
+  wallpaper = pkgs.wallpapers.aenami-northern-lights;
+  colorscheme.type = "rainbow";
+
 
   #  -----   ------
-  # | DP-1| | DP-2 |
+  # | DVI-I-1 | | HDMI-A-1 |
   #  -----   ------
   monitors = [
     {
-      name = "DP-1";
+      name = "DVI-I-1";
       width = 1920;
       height = 1080;
       workspace = "1";
       primary = true;
     }
     {
-      name = "DP-2";
+      name = "HDMI-A-1";
       width = 1920;
       height = 1080;
       position = "auto-right";
