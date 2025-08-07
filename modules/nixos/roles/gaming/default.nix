0215b9545780ac8a73d09fa6b09770a6409dd0c2
Alexander Sidorenko <aleks-sidorenko@users.noreply.github.com>
2025-08-07 15:16:09 +0300

    fix: gaming role mesa.drivers deprecation warning #pr (#31)

--- modules/nixos/roles/gaming/default.nix
+++ modules/nixos/roles/gaming/default.nix
@@ -19,10 +19,9 @@ in
     hardware = {
       # xpadneo.enable = true;
       xone.enable = true;
-      opengl = {
+      graphics = {
         enable = true;
-        driSupport = true;
-        driSupport32Bit = true;
+        enable32Bit = true;
         extraPackages = with pkgs; [
           mesa
         ];
