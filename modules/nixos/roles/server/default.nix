d5ab3b495cdb39c636913487cba63f05b67404b8
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2025-09-20 17:23:13 +0300

    fixed: minidlna

--- modules/nixos/roles/server/default.nix
+++ modules/nixos/roles/server/default.nix
@@ -93,10 +93,16 @@ in
       };
     };
 
-    # use TCP BBR has significantly increased throughput and reduced latency for connections
+    
     boot.kernel.sysctl = {
-      "net.core.default_qdisc" = "fq";
+      # Use TCP BBR has significantly increased throughput and reduced latency for connections
+      "net.core.default_qdisc" = "fq";      
       "net.ipv4.tcp_congestion_control" = "bbr";
+
+      # Default is usually 8192, increase to handle large media libraries
+      "fs.inotify.max_user_watches" = 524288;
+      # Also increase max instances if needed
+      "fs.inotify.max_user_instances" = 256;
     };
 
   };
