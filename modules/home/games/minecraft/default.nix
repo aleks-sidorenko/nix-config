8b847f394ce781be9b0de07339f162203701a785
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2025-10-18 13:13:32 +0300

    feat: gaming

---
+++ modules/home/games/minecraft/default.nix
@@ -0,0 +1,31 @@
+{
+  inputs,
+  lib,
+  host,
+  pkgs,
+  config,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace};
+let
+  cfg = config.${namespace}.games.minecraft;
+
+in
+{
+  options.${namespace}.games.minecraft = {
+    enable = mkEnableOption "Enable or disable Minecraft.";
+    
+  };
+
+  config = mkIf cfg.enable {
+
+    home = {
+      packages = with pkgs; [
+        prismlauncher
+      ];
+    };
+  };
+
+}
