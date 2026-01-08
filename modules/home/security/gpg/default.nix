1b74f36e1e16d3ee830dcef4171aa15043ab7dc8
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2026-01-08 19:54:14 +0200

    feat(gpg): use cli-based pinentry

--- modules/home/security/gpg/default.nix
+++ modules/home/security/gpg/default.nix
@@ -38,7 +38,7 @@ in
       sshKeys = cfg.sshKeys;
       defaultCacheTtl = 1800;
       defaultCacheTtlSsh = 1800;
-      pinentry.package = if config.gtk.enable then pkgs.pinentry-gnome3 else pkgs.pinentry-tty;
+      pinentry.package = pkgs.pinentry-curses;
       extraConfig = ''
         allow-preset-passphrase
         ttyname $GPG_TTY
