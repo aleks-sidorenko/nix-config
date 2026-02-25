3a1fdd46dd9e108626379b5380712ea3c4f42b08
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2026-02-25 19:37:42 +0200

    fix: ci

--- modules/nixos/cli/shells/default/default.nix
+++ modules/nixos/cli/shells/default/default.nix
@@ -14,17 +14,10 @@ in
 {
   options.${namespace}.cli.shells.default = with types; {
     enable = mkEnableOption "Whether or not to enable the default shell configuration";
-    name = mkStringOpt' "The name of the default shell to use";
-    package = mkPackageOpt' "The package to use for the default shell";
+    name = mkStringOpt "bash" "The name of the default shell to use";
+    package = mkPackageOpt pkgs.bash "The package to use for the default shell";
   };
 
-  config = mkIf cfg.enable {
-    assertions = [
-      {
-        assertion = cfg.name != null && cfg.package != null;
-        message = "Please specify a shell name and package in ${namespace}.cli.shells.default";
-      }
-    ];
-  };
+  config = mkIf cfg.enable { };
 
 }
