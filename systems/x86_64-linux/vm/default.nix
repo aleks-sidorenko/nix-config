d6ecce9d5fb86460ec63b4038c34255c47b57d69
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2026-03-02 18:27:02 +0200

    fix(vm): build

--- systems/x86_64-linux/vm/default.nix
+++ systems/x86_64-linux/vm/default.nix
@@ -33,7 +33,9 @@ in
 
   boot = {
 
-    kernelPackages = pkgs.linuxPackages_latest;
+    # There is issue with latest 6.19 kernel & nvidia drivers https://github.com/nixos/nixpkgs/issues/489947
+    # TODO - move to latest once it is resolved
+    kernelPackages = pkgs.linuxPackages_6_18; # pkgs.linuxPackages_latest;
 
   };
 
