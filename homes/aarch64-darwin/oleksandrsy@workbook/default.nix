40a37d9e20ca61722affcb2b669ff687f4cd349d
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2026-03-26 15:58:47 +0200

    feat(workbook): assign workspaces 6-10 to external monitor

--- homes/aarch64-darwin/oleksandrsy@workbook/default.nix
+++ homes/aarch64-darwin/oleksandrsy@workbook/default.nix
@@ -5,10 +5,15 @@
 }:
 with lib;
 with lib.${namespace};
+let
+  externalMonitor = "DELL U2419H";
+in
 {
   # TODO - replace with ${namespace} once this is fixed https://github.com/snowfallorg/lib/issues/142
   nix-config = {
     roles.work = enabled;
+
+    desktops.aerospace.workspaceMonitorAssignment = genAttrs [ "6" "7" "8" "9" "10" ] (_: externalMonitor);
 
     user = {
       enable = true;
