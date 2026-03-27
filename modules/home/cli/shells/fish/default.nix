cae584b04018fa76b8b2ea8efc4785d3613728bc
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2026-03-27 22:26:46 +0200

    fix: restore fish autosuggestion keybindings in vi insert mode
    fzf-fish plugin overrides Ctrl+F and Alt+F bindings, breaking
    accept-autosuggestion and forward-word in vi insert mode.

--- modules/home/cli/shells/fish/default.nix
+++ modules/home/cli/shells/fish/default.nix
@@ -64,6 +64,10 @@ in
         set fish_cursor_replace_one underscore blink
         set fish_cursor_visual      block
 
+        # Restore autosuggestion bindings overridden by fzf-fish plugin
+        bind -M insert \cf accept-autosuggestion
+        bind -M insert \ef forward-word
+
         # Correct cursor for ghostty when in VI mode.
         if string match -q -- '*ghostty*' $TERM
           set -g fish_vi_force_cursor 1
