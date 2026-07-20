47d0d136eae91f4b26db9aed01c0eac345ef4ad6
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2026-07-20 16:12:59 +0300

    feat(desktop,workbook): enable parent role as safe (non-blocked) control points

--- homes/aarch64-darwin/oleksandrsy@workbook/default.nix
+++ homes/aarch64-darwin/oleksandrsy@workbook/default.nix
@@ -9,6 +9,7 @@ with lib.${namespace};
   # TODO - replace with ${namespace} once this is fixed https://github.com/snowfallorg/lib/issues/142
   nix-config = {
     roles.work = enabled;
+    roles.parent = enabled;
 
     desktops.monitors = {
       enable = true;
